4b619ebe0c2ca3a21687657bc7173f64dec955d2
canoasel <212616187+SeleneLea@users.noreply.github.com>
2025-07-29 07:43:29 -0400

    Arreglos del Login y Register
    Sin implementacion del logo

--- productos/forms.py
+++ productos/forms.py
@@ -1,4 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class RegistroForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(RegistroForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = "Usuario"
+
+class LoginForm(AuthenticationForm):
     pass
